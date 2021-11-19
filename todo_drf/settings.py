cba0102c78c9aa1df5070303ece7d006c9607b24
smit1 <harpalsmit4333@gmail.com>
2021-11-18 18:10:32 -0800

    .

--- todo_drf/settings.py
+++ todo_drf/settings.py
@@ -62,7 +62,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'frontend/build')
+            os.path.join(BASE_DIR, 'build')
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -130,7 +130,7 @@ STATIC_URL = '/static/'
 
 
 STATICFILES_DIRS = [
-     os.path.join(BASE_DIR, 'frontend/build/static')
+     os.path.join(BASE_DIR, 'build/static')
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
