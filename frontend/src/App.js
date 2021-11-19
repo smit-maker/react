e95b71da64f6c993231a07650a739e2707e679b9
smit1 <harpalsmit4333@gmail.com>
2021-11-18 18:41:56 -0800

    .

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import logo from "./logo.svg";
 import test from "./test.png";
-import { Button } from 'react-bootstrap'
-import makeschool from "./makeschool.png"
+import { Button } from "react-bootstrap";
+import makeschool from "./makeschool.png";
 // import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
@@ -16,17 +16,8 @@ const App = () => {
       <img src={makeschool} alt="TEST image" />
       <hr></hr>
       <h2>Test Bootstrap Buttons</h2>
-      <Button bsStyle="primary">Primary</Button>{' '}
-      <Button type="button" class="btn btn-primary">Secondary</Button>{' '}
-      <Button variant="success">Success</Button>{' '}
-      <Button variant="warning">Warning</Button>{' '}
-
-      <button type="button" class="btn btn-primary">Primary BootStrap</button>
-<button type="button" class="btn btn-secondary">Secondary Bootstap</button>
-
     </div>
-
-  )
+  );
 };
 
 export default App;
